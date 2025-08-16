cbf0f16a961e4e16151de7d7564a6383e8d5d889
Pavarit Pramual <pavarit.dev@gmail.com>
2025-08-16 21:06:41 +0700

    feat: add menu and main loop in main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,31 @@
 from textbank import all_text
 
-print(all_text)
+while True:
+  print("\n-- Menu --")
+  print("1. Typing Speed Test 15 Seconds")
+  print("2. Typing Speed Test 30 Seconds")
+  print("3. Typing Speed Test 1 Minute")
+  print("4. Exit Program")
+
+  choice = input("Please select an option (1-4): ")
+
+  if choice == "1":
+    print("\n[Typing Speed Test 15 Seconds Starting...]")
+
+    print("[Typing Test Finished! Returning to Menu...]")
+
+  elif choice == "2":
+    print("\n[Typing Speed Test 30 Seconds Starting...]")
+
+    print("[Typing Test Finished! Returning to Menu...]")
+
+  elif choice == "3":
+    
+    print("[Typing Test Finished! Returning to Menu...]")
+
+  elif choice == "4":
+    print("\nExiting Program...")
+    break
+
+  else:
+    print("\nPlease Input The Required Number")
