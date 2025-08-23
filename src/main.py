1b82d5d7d2e89b1d4a926851c19ea86e10eea2ae
Teeramet Saikham <thirameth.saykham2548@gmail.com>
2025-08-23 23:07:24 +0700

    create randoming sentence, count correct and in correct

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from textbank import all_text
+import random
 print("=== Typing Speed Meter (Batch) ===")
 print("Hi! This program measures your typing speed and accuracy.")
 while True:
@@ -8,13 +9,27 @@ while True:
 
   choice = input("Please select an option (1-2): ")
 
-  if choice == "1":
+  if choice == "1": 
     print("\nPrompt selected:")
-    print("Consistent practice improves both speed and accuracy.")
+    # *** Random sentence ***
+    random_sentence = random.choice(all_text) # Normal random it can be same sentence.
+    # random_sentence = random.shuffle(all_text) -> It can't be same sentence.
+    print(random_sentence)
     print("\nPress Enter to START timing...")
     user_input = input("")
     print("\nType the prompt and press enter to SUBMIT:")
     user_input = input(">> ")
+    correct_count = 0
+    incorrect_count = 0
+    # *** it check only correct and incorrect. If error out of range because: you input more or less than random text ***
+    for char in range(len(random_sentence) - 1):
+      if user_input[char] == random_sentence[char]:
+        correct_count += 1
+      else:
+        incorrect_count += 1
+    print("Correct :",correct_count,"Incorrect :",incorrect_count)
+
+
 
   elif choice == "2":
     print("\nExiting Program...")
